14b2c6bcbcc7b92687a0f5fdbd99dc7e1aeb8db3
EngAmanyAhmed <eng.amanyahmed.81@gmail.com>
2025-03-09 16:46:50 +0200

    Add project files.

---
+++ Company.G02.BLL/Repositories/DepartmentRepository.cs
@@ -0,0 +1,45 @@
+﻿using Company.G02.BLL.Interfaces;
+using Company.G02.DAL.Data.Context;
+using Company.G02.DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Company.G02.BLL.Repositories
+{
+    public class DepartmentRepository : IDepartmentRepository
+    {
+        private readonly CompanyDbContext _context; //Null
+        //Asdk Clr to create an object from CompanyDbContext
+        public DepartmentRepository(CompanyDbContext context)
+        {
+            _context = context;
+        }
+        public IEnumerable<Department> GetAll()
+        {
+            return _context.Departments.ToList();
+        }
+        public Department? GetById(int id)
+        {
+            return _context.Departments.Find(id);
+        }
+        public int Add(Department department)
+        {
+            _context.Add(department);
+            return _context.SaveChanges();
+        }
+        public int Update(Department department)
+        {
+            _context.Update(department);
+            return _context.SaveChanges();
+        }
+        public int Delete(Department department)
+        {
+            _context.Remove(department);
+            return _context.SaveChanges();
+        }
+       
+    }
+}
---
+++ Company.G02.PL/Controllers/DepartmentController.cs
@@ -0,0 +1,68 @@
+﻿using Company.G02.BLL.Interfaces;
+using Company.G02.BLL.Repositories;
+using Company.G02.DAL.Models;
+using Company.G02.DAL.Models.Dtos;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Company.G02.PL.Controllers
+{
+    //Mvc Controller
+    public class DepartmentController : Controller
+    {
+        private readonly IDepartmentRepository _departmentRepository;
+        //Ask Clr to create an object from DepartmentRepository
+        public DepartmentController(IDepartmentRepository departmentRepository)
+
+        {
+            _departmentRepository=departmentRepository;
+        }
+
+        [HttpGet] // Get : /Department/Index
+        public IActionResult Index()
+        {
+            var departments = _departmentRepository.GetAll();
+
+
+            return View(departments);
+        }
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(CreateDepartmentDto model)
+        {
+            if ( ModelState.IsValid) //Server side validation
+            {   
+                var department=new Department() 
+                {
+                  Code = model.Code,
+                  Name = model.Name,
+                  CreatedAt = model.CreatedAt,
+                };
+              var count= _departmentRepository.Add(department);
+                if (count > 0) 
+                {
+                  return RedirectToAction(nameof(Index));
+                }
+            }
+            return View(model);
+        }
+
+        [HttpGet]
+        public IActionResult Details(DetailsDepartmentDto departmentDto) 
+        {
+            var department = _departmentRepository.GetById(departmentDto.Id);
+            var departmentDetailsDto = new DetailsDepartmentDto
+            {
+                Code= department.Code,
+                Name = department.Name,
+                CreatedAt = department.CreatedAt,
+            };
+
+            return View(departmentDetailsDto);
+        }
+    }
+}
